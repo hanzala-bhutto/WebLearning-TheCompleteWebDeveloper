3166897429d75177c0cb574b4799ca7f3eaa214a
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-09-17 14:12:38 +0500

    started exercise 7 javascript

---
+++ Section 11 JavaScript/exercise7/exercise7Answer.js
@@ -0,0 +1,31 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+{
+    username: "Hero",
+    password: "Zero"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {
+        username: "Hero",
+        password: "Zero"
+    }
+]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+newsfeed = [
+    {
+        username: "Java",
+        timeline: "Java is hyped"
+    },
+    {
+        username: "Python",
+        timeline: "Python is all-rounder"
+    },
+    {
+        username: "C++",
+        timeline: "C++ is too old - but they say old is gold"
+    }
+]
