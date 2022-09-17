a3ab574a7a15e1231d38780fb9f2cffa9077a914
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-09-17 11:15:27 +0500

    completed function challenge

---
+++ Section 11 JavaScript/exercise5/exercise5Answer.js
@@ -0,0 +1,37 @@
+function checkDriverAge(age){
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+function checkDriverAgeConsole(age){
+    if (Number(age) < 18) {
+        return "Sorry, you are too yound to drive this car. Powering off";
+    } else if (Number(age) > 18) {
+        return "Powering On. Enjoy the ride!";
+    } else if (Number(age) === 18) {
+        return "Congratulations on your first year of driving. Enjoy the ride!";
+    }
+}
+
+var age = prompt("What is your age?");
+
+
+var checkDriverAge2 = function(age){
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+
+checkDriverAge(age)
+checkDriverAge2(age)
+checkDriverAgeConsole(90)
