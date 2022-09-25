60eb855b71bd6c2fca12d27c0377114c8f63ce8c
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-09-25 09:26:11 +0500

    completed advanced javascript exercise 7

--- Section 13 Advanced Javascript/advanced_exercise_es7/exerciseES7-SOLUTIONS.js
+++ Section 13 Advanced Javascript/advanced_exercise_es7/exerciseES7-SOLUTIONS.js
@@ -1,21 +1,20 @@
 // Solve the below problems:
 
-
 // #1) Check if this array includes the name "John".
-const dragons1 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-console.log(dragons1.includes('John'));
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
+dragons.includes('John') // false
 
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
-dragonsfilter = dragons.filter((name) => {
-    return name.includes('John');
-})
-console.log(dragonsfilter);
+
+dragons.filter(name => name.includes('John')) // ['Johnathan']
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
-powerOf100 = (x) => x**100;
+const power100 = (num) => num**100;
 
 // #4) Useing your function from #3, put in the paramter 10000. What is the result?
-// Research for yourself why you get this result
-console.log(powerOf100(10000))
+// Research for yourself why you get this result.
+
+power100(10000) // Infinity
