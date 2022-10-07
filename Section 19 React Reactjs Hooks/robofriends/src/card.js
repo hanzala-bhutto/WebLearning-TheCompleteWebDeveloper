d8611a8ec91e2b756d7e36689549f62ef838c154
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-07 12:28:36 +0500

    styled card component using tachyon

--- Section 19 React Reactjs Hooks/robofriends/src/card.js
+++ Section 19 React Reactjs Hooks/robofriends/src/card.js
@@ -4,7 +4,7 @@ const card = (props) => {
     // destructuring
     const {id,name,email} = props;
     return (
-        <div>
+        <div className="tc bg-light-green dib pr3 ba3 ma2 grow bw2 shadow-5">
             <img alt='robot' src={`https://robohash.org/${id}?200x200`}></img>
             <div>
                 <h2>{name}</h2>
