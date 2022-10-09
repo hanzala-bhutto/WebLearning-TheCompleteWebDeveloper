ee129fe86911acb22c4b1aca7580cf69d5861e77
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-09 14:20:26 +0500

    the concept of mounting updating and unmounting in react

--- Section 19 React Reactjs Hooks/robofriends/src/App.js
+++ Section 19 React Reactjs Hooks/robofriends/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import {robots} from './robots';
+// import {robots} from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+
+
+// Concepts of Life Cycle Hooks
+// Mounting
+// Updating
+// UnMounting
 
 class App extends Component{
 
     constructor(){
         super();
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
+    }
+
+    // Mounting
+    // Using jsonplaceholder api endpoint to get json data of 10 users
+    // If application has started then change the state of robots field
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users=> this.setState({robots:users}));
     }
 
     // whenever creating custom method, use arrow function to properly indicate where is the event taking place
@@ -24,15 +39,27 @@ class App extends Component{
         const filteredRobots = this.state.robots.filter((robot) => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        // communicate OnSeachChange method to SearchBox
-        // communicate filterRobots to CardList
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/> 
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+
+        if (this.state.robots.length === 0){
+            return (
+                <div className='container'>
+                    <h1 className='f1'>Loading ...</h1>
+                </div>
+          )
+        }
+        else{
+            // communicate OnSeachChange method to SearchBox
+            // communicate filterRobots to CardList
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/> 
+                    <CardList robots={filteredRobots}/>
+                </div>
+            )
+        }
+
+
     }
 }
 
