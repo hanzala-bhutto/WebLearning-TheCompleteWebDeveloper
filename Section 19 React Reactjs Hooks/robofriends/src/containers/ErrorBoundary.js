ed9cae38a01f007fbcb5f21a13498e4d47b6494f
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-10 18:06:01 +0500

    Error boundary to catch error in components

---
+++ Section 19 React Reactjs Hooks/robofriends/src/containers/ErrorBoundary.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            hasError: false
+        }
+    }
+
+    render(){
+
+    }
+}
