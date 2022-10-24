23331b30ecfaf729bb54111c1b80db6c4fea5d51
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-24 20:41:15 +0500

    ImageLinkForm skeleton set

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/ImageLinkForm/ImageLinkForm.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,8 +1,18 @@
 import React from 'react';
-
+import './ImageLinkForm.css';
 const ImageLinkForm = () => {
     return (
-        <div></div>
+        <div>
+            <p className='f3 tc meow'>
+                This Magic brain will detect faces in your pictures. Give it a try
+            </p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center b--none' type='text' />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white b--none bg-light-purple '>Detect</button>
+                </div>  
+            </div>
+        </div>
     );
 }
 
