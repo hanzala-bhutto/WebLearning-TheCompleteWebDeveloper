5ba4c1b0556415166de33a83248f36124da4c00a
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-25 10:23:04 +0500

    face recognition component skeleton set, clarifie api

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
@@ -1,9 +1,16 @@
 import { Component } from "react";
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+  apiKey: '4cae014b57964844b259fe0a988be7e9'
+ });
+ 
 
 class App extends Component {
 
@@ -18,8 +25,18 @@ class App extends Component {
     console.log(event.target.value);
   }
 
-  onButtonSubmit = (event) => {
-    console.log("click");
+  onButtonSubmit = () => {
+    // console.log("click");
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,
+    "https://samples.clarifai.com/face-det.jpg")
+    .then(
+    function(response){
+        console.log(response);
+        console.log("click");
+    },
+    function(err){
+      console.log(err);
+    });
   }
 
   render() {
@@ -29,7 +46,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        {/* <FaceRecognition /> */}
+        <FaceRecognition />
         <ParticlesBg color="#ffffff" num={100} alpha={[0.9, 0]} type="cobweb" bg={true} />
       </div>
     )
---
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className='center tc'>
+            <img src="https://samples.clarifai.com/face-det.jpg" alt='face detection' />
+        </div>
+    )
+}
+
+export default FaceRecognition;
