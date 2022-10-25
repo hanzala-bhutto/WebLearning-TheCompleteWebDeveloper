592610d3b6f6c45d99c92507841310c23a2dca4a
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-25 18:55:31 +0500

    face border calculated from clarify api response

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
@@ -18,13 +18,28 @@ class App extends Component {
     super();
     this.state = {
       input : '',
-      imageUrl : ''
+      imageUrl : '',
+      box: {}
     };
   }
 
   calculateFaceLocation = (data) => {
-    console.log(data);
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const height = Number(image.height);
+    const width = Number(image.width);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    } 
+  
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box:box});
   }
 
   onInputChange = (event) => {
@@ -33,12 +48,9 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,
-    this.state.input)
-    .then(response => {
-      // console.log('hi', response)
-      this.calculateFaceLocation(response);
-    })
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
     .catch(err => console.log(err))
   }
 
--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/FaceRecognition/FaceRecognition.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const FaceRecognition = ({imageUrl}) => {
     return (
         <div className='center ma mt2'>
             {/* <div className='absolute mt2'> */}
-                <img src={imageUrl} width='500px' height='auto' alt='' />
+                <img id='inputImage' src={imageUrl} width='500px' height='auto' alt='' />
             {/* </div> */}
         </div>
     )
