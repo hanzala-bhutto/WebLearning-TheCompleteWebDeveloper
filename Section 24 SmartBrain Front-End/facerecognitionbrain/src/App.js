75e42c5043ec5ff4b1c177d0d2f36d6fe88fdeab
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-25 08:36:32 +0500

    state added in App to handle inputChange

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
@@ -6,13 +6,29 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input : ''
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = (event) => {
+    console.log("click");
+  }
+
   render() {
     return (
       <div>
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         {/* <FaceRecognition /> */}
         <ParticlesBg color="#ffffff" num={100} alpha={[0.9, 0]} type="cobweb" bg={true} />
       </div>
--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/ImageLinkForm/ImageLinkForm.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ImageLinkForm.css';
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3 tc'>
@@ -8,8 +8,10 @@ const ImageLinkForm = () => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center b--none' type='text' />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white b--none bg-light-purple'>Detect</button>
+                    <input className='f4 pa2 w-70 center b--none' type='text' onChange={onInputChange}/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white b--none bg-light-purple' onClick={onButtonSubmit}>
+                        Detect
+                    </button>
                 </div>  
             </div>
         </div>
