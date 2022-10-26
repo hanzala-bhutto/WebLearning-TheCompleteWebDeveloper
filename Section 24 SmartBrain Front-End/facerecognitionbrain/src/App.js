6aeaa166310db9e7f3385d6c3f32236ee96e35af
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-26 19:18:11 +0500

    routings for register set

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
@@ -67,15 +67,21 @@ class App extends Component {
         <ParticlesBg color="#ffffff" num={100} alpha={[0.9, 0]} type="cobweb" bg={true} />
         <Navigation onRouteChange={this.onRouteChange} />
         {
-        (this.state.route === 'signin')
-        ?<Signin onRouteChange={this.onRouteChange} />
-        :
+        (this.state.route === 'home')
+        ?        
         <div>
           <Logo />
           <Rank />
           <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
           <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
         </div>
+        :((this.state.route === 'signin')
+          ?
+          <Signin onRouteChange={this.onRouteChange} />
+          :
+          <Register onRouteChange={this.onRouteChange} />
+          )
+
         }
       </div>
     )
