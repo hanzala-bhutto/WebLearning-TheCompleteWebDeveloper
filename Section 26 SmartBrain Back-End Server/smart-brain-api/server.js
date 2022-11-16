689bec61c43b2d9ebe7be4e5b8986c79a3195be7
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-11-16 21:20:31 +0500

    created signin and register server requests

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email:'john@gmail.com',
+            password:'cookies',
+            entries:'0',
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email:'sally@gmail.com',
+            password:'bananas',
+            entries:'0',
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req,res) => {
-    res.send("this is working");
+    res.send(database.users);
+})
+
+app.post('/signin', (req,res) => {
+    if(req.body.email === database.users[0].email
+        && req.body.password === database.users[0].password
+    ){
+        res.json("Success");
+    }
+    else{
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req,res) => {
+    
+    const {name,email,password} = req.body;
+    database.users.push(
+    {
+        id : 125,
+        name:name,
+        email:email,
+        password: password,
+        entires: 0,
+        joined: new Date()
+    }
+    )
+    res.send("registered");
+
 })
 
 app.listen(3000, ()=>{
@@ -12,5 +63,6 @@ app.listen(3000, ()=>{
 
 /*
 res = this is working
-
+signin = post
+register = post
 */
