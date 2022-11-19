07b0ab2076bd16b71463c4671e2e9f1d5018aa2f
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-11-19 11:10:41 +0500

    npm cors to bypass "Access Control Allow Origin" and communicating frontend with server

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 const database = {
     users: [
@@ -100,7 +103,7 @@ app.post('/register', (req,res) => {
     res.json(database.users[database.users.length-1]);
 })
 
-app.listen(3000, ()=>{
+app.listen(3001, ()=>{
     console.log("App is running");
 })
 
