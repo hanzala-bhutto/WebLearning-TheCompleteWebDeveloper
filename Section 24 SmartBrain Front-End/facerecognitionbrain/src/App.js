927c9d25f57b34c7459dd4213e1a3016b150d2ea
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-01-16 12:51:21 +0500

    rank entries fetch from server upon face detection and incremented

--- Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
+++ Section 24 SmartBrain Front-End/facerecognitionbrain/src/App.js
@@ -84,7 +84,6 @@ class App extends Component {
           type: 'visual-detector',
         }, this.state.input)
       .then(response => {
-        console.log('hi', response)
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
