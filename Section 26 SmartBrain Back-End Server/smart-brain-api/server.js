5ffd8004112d8902118dfcb5ddbafadc277c0ba9
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-01-16 21:09:39 +0500

    updating entries from database users table

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -87,17 +87,16 @@ app.get('/profile/:id', (req,res) => {
 
 app.put('/image', (req,res) => {
     const {id} = req.body;
-    let idFound = false;
-    database.users.forEach(user => {
-        if(user.id === id){
-            idFound=true;
-            user.entries++;
-            return res.json(user.entries);
-        }
+    
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
     })
-    if(!idFound){
-        res.status(400).json("user not found!");
-    }
+    .catch(err => {
+        res.status(400).json("unable to get entries!");
+    })
 })
 
 app.post('/register', (req,res) => {
