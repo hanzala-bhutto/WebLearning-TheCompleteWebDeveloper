e663f702768e530fb2033088e5a207c74afd4679
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-01-17 12:48:50 +0500

    signin controller

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -6,6 +6,7 @@ const app = express();
 const knex = require('knex');
 
 const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 
 const db = knex({
     client: 'pg',
@@ -29,28 +30,7 @@ app.get('/', (req,res) => {
     res.send('success');
 })
 
-app.post('/signin', (req,res) => {
-    const {email,password} = req.body;
-
-    db.select('email','hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-        const isValid  = bcrypt.compareSync(password, data[0].hash);
-        if(isValid){
-            return db.select('*').from('users')
-            .where('email', '=', email)
-            .then(user => {
-                console.log(user);
-                res.json(user[0]);
-            })
-            .catch(err => res.status(400).json('unable to get user'));
-        }
-        else{
-            res.status(400).json('wrong credentials');
-        }
-    })
-    .catch(err => res.status(400).json('wrong credentials'));
-})
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)});
 
 app.get('/profile/:id', (req,res) => {
     const {id} = req.params;
