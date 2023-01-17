1e4eab1f1540d240199bccb02e0ca46d64a1630a
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-01-17 12:43:51 +0500

    code review separation of concerns register handler

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 const knex = require('knex');
 
+const register = require('./controllers/register');
 
 const db = knex({
     client: 'pg',
@@ -81,36 +82,7 @@ app.put('/image', (req,res) => {
     })
 })
 
-app.post('/register', (req,res) => {
-    
-    const {name,email,password} = req.body;
-
-    const hash = bcrypt.hashSync(password);
-
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email:loginEmail[0].email,
-                name: name,
-                joined: new Date()
-            }).then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'));
-    
-})
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
 
 app.listen(3000, ()=>{
     console.log("App is running");
