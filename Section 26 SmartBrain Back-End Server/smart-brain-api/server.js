d6b539574fd01005c84a68b9ded5b998fa70bfaf
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2023-01-17 12:56:08 +0500

    server.js

--- Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
+++ Section 26 SmartBrain Back-End Server/smart-brain-api/server.js
@@ -36,7 +36,6 @@ app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)});
 
 app.get('/profile/:id', (req,res) => {profile.handleProfileGet(req,res,db)});
  
-
 app.put('/image', (req,res) => {image.handleImage(req,res,db)});
 
 app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
